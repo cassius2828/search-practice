ee64cbcbf823323ea8a9308e7cc2cdd6ae3545d1
Cassius Reynolds <cassius2828@gmail.com>
2023-08-01 19:57:54 -0700

    fixed syntax error, now card data is fetched properly

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,44 +1,44 @@
 import React from "react";
 import { Card } from "./Card";
-import '../App.css'
+import "../App.css";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-
+const url = "https://akabab.github.io/superhero-api/api/all.json";
 
 export const Display = () => {
-      const [heroData, setHeroData] = useState([]);
+  const [heroData, setHeroData] = useState([]);
+  const fetchHeros = async () => {
+    try {
+      const resp = await axios.get(url);
+      setHeroData(resp);
+    } catch (error) {
+      console.log("error occured", error);
+    }
+  };
 
-      // * API KEY: 6547873555272160
-      const fetchHeros = async () => {
-        try {
-          const resp = await axios.get(
-            "https://akabab.github.io/superhero-api/api/all.json"
-          );
-          setHeroData(resp);
-        } catch (error) {
-          console.log( 'error occured', error);
-        }
-      };
+  useEffect(() => {
+    fetchHeros();
+  }, []);
+  console.log(heroData);
 
-      useEffect(() => {
-        fetchHeros();
-      }, []);
-    //   console.log(heroData.data[34])
-
-      const fullList = heroData.data.map((name, id, images, biography) => {
-        return (
-          <>
-            <Card key={id} image={images.lg} character={name} publisher={biography.publisher} />
-          </>
-        );
-      })
+    const fullList = heroData.data.map(({name, id, images, biography}) => {
+      return (
+        <>
+          <Card
+            key={id}
+            image={images.lg}
+            character={name}
+            publisher={biography.publisher}
+          />
+        </>
+      );
+    });
   return (
     <div className="scroll-container">
-        {/* name, id, images.lg, biography.publisher, biography.alignment */}
-        {/* {heroData.data[34] ? fullList : <h1>APi wont retreive rn </h1>} */}
-        {fullList}
-      
+      {/* name, id, images.lg, biography.publisher, biography.alignment */}
+      {fullList}
     </div>
   );
 };
+// * API KEY: 6547873555272160
